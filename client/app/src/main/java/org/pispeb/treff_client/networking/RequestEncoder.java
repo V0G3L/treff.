abe80259c62c21758c9e38893f7f36c30e59e060
Matthias-Noll <33349636+Matthias-Noll@users.noreply.github.com>
2018-01-06 12:15:55 +0100

    added some requests to RequestEncoder

--- client/app/src/main/java/org/pispeb/treff_client/networking/RequestEncoder.java
+++ client/app/src/main/java/org/pispeb/treff_client/networking/RequestEncoder.java
@@ -1,5 +1,7 @@
 package org.pispeb.treff_client.networking;
 
+import org.pispeb.treff_client.entities.Poll;
+
 /**
  * Created by Lukas on 1/5/2018.
  */
@@ -40,9 +42,40 @@ public class RequestEncoder {
         return false;
     }
 
+    public Integer[] listContacts() { return null;}
+
     public boolean blockAccount(int userId) {
         return false;
     }
 
-    public boolean update_position(int lat, int lon, int time) { return false; }
+    public boolean unblockAccount(int userId) {
+        return false;
+    }
+
+    public int createGroup(String groupName, Integer[] memberIds) { return 0; }
+
+    public boolean editGroupName(int groupId, String name) { return false; }
+
+    public boolean addGroupMembers(int groupId, Integer[] newMembers) { return false; }
+
+    public boolean removeGroupMembers(int groupId, Integer[] members) { return false; }
+
+    public int createEvent(int groupId, String title, int timeStart,
+                           int timeEnd, int lat, int lon) {
+        return 0;
+    }
+
+    //public boolean editEvent(int groupId)
+
+    public boolean removeEvent(int groupId, int eventId) { return false; }
+
+    public Poll createPoll(int groupId, String question, boolean multichoice, String[] options) {
+        return null;
+    }
+
+    //public boolean editPoll(int groupId, int pollId, )
+
+    public boolean removePoll(int groupId, int pollId) { return false; }
+
+    public boolean updatePosition(int lat, int lon, int time) { return false; }
 }
