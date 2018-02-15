bd2fae44d2c0ba2eda8df5152d8301bd17e00361
V0G3L <s30m4nv0g3l@gmail.com>
2018-02-15 22:10:29 +0100

    Wrote all commands for Contact requests

--- server/treff_server/src/main/java/org/pispeb/treff_server/commands/RejectContactRequestCommand.java
+++ server/treff_server/src/main/java/org/pispeb/treff_server/commands/RejectContactRequestCommand.java
@@ -1,9 +1,14 @@
 package org.pispeb.treff_server.commands;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.pispeb.treff_server.commands.io.CommandInput;
+import org.pispeb.treff_server.commands.io.CommandInputLoginRequired;
 import org.pispeb.treff_server.commands.io.CommandOutput;
+import org.pispeb.treff_server.commands.io.ErrorOutput;
 import org.pispeb.treff_server.exceptions.DatabaseException;
+import org.pispeb.treff_server.interfaces.Account;
 import org.pispeb.treff_server.interfaces.AccountManager;
+import org.pispeb.treff_server.networking.ErrorCode;
 
 /**
  * a command to reject a received contact request
@@ -12,13 +17,57 @@ public class RejectContactRequestCommand extends AbstractCommand {
 
     public RejectContactRequestCommand(AccountManager accountManager) {
         super(accountManager, CommandInput.class);
-        throw new UnsupportedOperationException();
     }
 
     @Override
-    protected CommandOutput executeInternal(CommandInput commandInput) throws
-            DatabaseException {
-        return null; //TODO
+    protected CommandOutput executeInternal(CommandInput commandInput) {
+        Input input = (Input) commandInput;
+        Account actingAccount;
+        Account newContact;
+
+        // get accounts
+        if (input.getActingAccount().getID() < input.id) {
+            actingAccount = getSafeForWriting(input.getActingAccount());
+            newContact = getSafeForWriting(
+                    accountManager.getAccount(input.id));
+        } else {
+            newContact = getSafeForWriting(
+                    accountManager.getAccount(input.id));
+            actingAccount = getSafeForWriting(input.getActingAccount());
+        }
+        if (actingAccount == null) {
+            return new ErrorOutput(ErrorCode.TOKENINVALID);
+        }
+        if (newContact == null) {
+            return new ErrorOutput(ErrorCode.USERIDINVALID);
+        }
+
+        // check if request exist
+        if (!actingAccount.getAllIncomingContactRequests()
+                .containsKey(input.id)) {
+            return new ErrorOutput(ErrorCode.NOCONTACTREQUEST);
+        }
+
+        // reject request
+        actingAccount.rejectContactRequest(newContact);
+
+        return new Output();
+    }
+
+    public static class Input extends CommandInputLoginRequired {
+
+        final int id;
+
+        public Input(@JsonProperty("id") int id,
+                     @JsonProperty("token") String token) {
+            super(token);
+            this.id = id;
+        }
+    }
+
+    public static class Output extends CommandOutput {
+        Output() {
+        }
     }
 
 }
--- server/treff_server/src/main/java/org/pispeb/treff_server/commands/SendContactRequestCommand.java
+++ server/treff_server/src/main/java/org/pispeb/treff_server/commands/SendContactRequestCommand.java
@@ -1,9 +1,13 @@
 package org.pispeb.treff_server.commands;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.pispeb.treff_server.commands.io.CommandInput;
+import org.pispeb.treff_server.commands.io.CommandInputLoginRequired;
 import org.pispeb.treff_server.commands.io.CommandOutput;
-import org.pispeb.treff_server.exceptions.DatabaseException;
+import org.pispeb.treff_server.commands.io.ErrorOutput;
+import org.pispeb.treff_server.interfaces.Account;
 import org.pispeb.treff_server.interfaces.AccountManager;
+import org.pispeb.treff_server.networking.ErrorCode;
 
 /**
  * a command to send a contact request to another user/account
@@ -12,13 +16,66 @@ public class SendContactRequestCommand extends AbstractCommand {
 
     public SendContactRequestCommand(AccountManager accountManager) {
         super(accountManager, CommandInput.class);
-        throw new UnsupportedOperationException();
     }
 
     @Override
-    protected CommandOutput executeInternal(CommandInput commandInput) throws
-            DatabaseException {
-        return null; //TODO
+    protected CommandOutput executeInternal(CommandInput commandInput) {
+        Input input = (Input) commandInput;
+        Account actingAccount;
+        Account newContact;
+
+        // get accounts
+        if (input.getActingAccount().getID() < input.id) {
+            actingAccount = getSafeForWriting(input.getActingAccount());
+            newContact = getSafeForWriting(
+                    accountManager.getAccount(input.id));
+        } else {
+            newContact = getSafeForWriting(
+                    accountManager.getAccount(input.id));
+            actingAccount = getSafeForWriting(input.getActingAccount());
+        }
+        if (actingAccount == null) {
+            return new ErrorOutput(ErrorCode.TOKENINVALID);
+        }
+        if (newContact == null) {
+            return new ErrorOutput(ErrorCode.USERIDINVALID);
+        }
+
+        // check blocks
+        if (actingAccount.getAllBlocks().containsKey(input.id)) {
+            return new ErrorOutput(ErrorCode.BLOCKINGALREADY);
+        }
+
+        if (newContact.getAllBlocks()
+                .containsKey(input.getActingAccount().getID())) {
+            return new ErrorOutput(ErrorCode.BEINGBLOCKED);
+        }
+
+        // check if request was already sent
+        if (actingAccount.getAllOutgoingContactRequests()
+                .containsKey(input.id)) {
+            return new ErrorOutput(ErrorCode.CONTACTREQUESTPENDING);
+        }
+
+        // send request
+        actingAccount.sendContactRequest(newContact);
+
+        return new Output();
     }
 
+    public static class Input extends CommandInputLoginRequired {
+
+        final int id;
+
+        public Input(@JsonProperty("id") int id,
+                     @JsonProperty("token") String token) {
+            super(token);
+            this.id = id;
+        }
+    }
+
+    public static class Output extends CommandOutput {
+        Output() {
+        }
+    }
 }
