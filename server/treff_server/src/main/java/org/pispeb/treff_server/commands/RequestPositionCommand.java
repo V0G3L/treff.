0d12bd419d7d778aa42ff1b1764ab561a332882f
Jens Kienle <jens_kienle@aol.de>
2018-02-16 01:47:02 +0100

    removed AddContactCommand; added some Updates to the protocol; started implementing RequestPositionCommand

--- server/treff_server/src/main/java/org/pispeb/treff_server/commands/RequestPositionCommand.java
+++ server/treff_server/src/main/java/org/pispeb/treff_server/commands/RequestPositionCommand.java
@@ -1,9 +1,16 @@
 package org.pispeb.treff_server.commands;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.pispeb.treff_server.commands.io.CommandInput;
+import org.pispeb.treff_server.commands.io.CommandInputLoginRequired;
 import org.pispeb.treff_server.commands.io.CommandOutput;
-import org.pispeb.treff_server.exceptions.DatabaseException;
+import org.pispeb.treff_server.commands.io.ErrorOutput;
+import org.pispeb.treff_server.interfaces.Account;
 import org.pispeb.treff_server.interfaces.AccountManager;
+import org.pispeb.treff_server.interfaces.Usergroup;
+import org.pispeb.treff_server.networking.ErrorCode;
+
+import java.util.Date;
 
 /**
  * a command to request the Position of an Account
@@ -11,14 +18,45 @@ import org.pispeb.treff_server.interfaces.AccountManager;
 public class RequestPositionCommand extends AbstractCommand {
 
     public RequestPositionCommand(AccountManager accountManager) {
-        super(accountManager, CommandInput.class);
-		throw new UnsupportedOperationException();
+        super(accountManager, Input.class);
     }
 
     @Override
-    protected CommandOutput executeInternal(CommandInput commandInput) throws
-            DatabaseException {
+    protected CommandOutput executeInternal(CommandInput commandInput) {
+        Input input = (Input) commandInput;
+
+        // get account and check if it still exists
+        Account actingAccount =
+                getSafeForReading(input.getActingAccount());
+        if (actingAccount == null) {
+            return new ErrorOutput(ErrorCode.TOKENINVALID);
+        }
+
+        // get group
+        Usergroup group =
+                getSafeForReading(actingAccount.getAllGroups()
+                        .get(input.groupId));
+        if (group == null) {
+            return new ErrorOutput(ErrorCode.GROUPIDINVALID);
+        }
+
         return null; //TODO
     }
 
+    public static class Input extends CommandInputLoginRequired {
+
+        final int groupId;
+        final Date time;
+
+        public Input(@JsonProperty("id") int groupId,
+                     @JsonProperty("time") long time,
+                     @JsonProperty("token") String token) {
+            super(token);
+            this.groupId = groupId;
+            this.time = new Date(time);
+        }
+    }
+
+    public static class Output extends CommandOutput {
+    }
 }
